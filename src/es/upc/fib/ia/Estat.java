4054460a4a2ae07df789ef8243171f3b7b3940a6
aleixsacrest <aleixsacrest@gmail.com>
2016-03-14 14:10:32 +0100

    estat getters, setters, operadors

--- src/es/upc/fib/ia/Estat.java
+++ src/es/upc/fib/ia/Estat.java
@@ -4,13 +4,56 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 /**
- * Created by Msacrest on 08/03/2016.
+ * Created by aleixsacrest on 08/03/2016.
  */
 public class Estat {
-    private HashMap<Integer, HashSet<Integer>> servidors;
-    private int[] peticions;
+    //TODO: public o privat
+    public HashMap<Integer, HashSet<Integer>> servidors;
+    public int[] peticions;
 
     public Estat() {
         servidors = new HashMap<Integer, HashSet<Integer>>();
+        //peticions = new int[]
+    }
+
+    public void setServidors(HashMap<Integer, HashSet<Integer>> s) {
+        this.servidors = s;
+    }
+
+    public HashMap<Integer, HashSet<Integer>> getServidors() {
+        return this.servidors;
+    }
+
+    public void setPeticions(int[] p) {
+        this.peticions = p;
+    }
+
+    public int[] getPeticions() {
+        return this.peticions;
+    }
+
+    public HashSet<Integer> getAssignacions(int IDserv) {
+        return this.servidors.get(IDserv);
+    }
+
+    public int getServei(int IDpet) {
+        return this.peticions[IDpet];
+    }
+
+    public void canviarAssignacio(int IDpeticio, int IDnou) {
+        int IDantic = this.peticions[IDpeticio];
+        this.servidors.get(IDantic).remove(IDpeticio);
+        this.servidors.get(IDnou).add(IDpeticio);
+    }
+
+    public void intervanviarAssignacions(int IDpet1, int IDpet2) {
+        int IDserv1 = peticions[IDpet1];
+        int IDserv2 = peticions[IDpet2];
+        this.peticions[IDpet1] = IDserv2;
+        this.peticions[IDpet2] = IDserv1;
+        this.servidors.get(IDserv1).remove(IDpet1);
+        this.servidors.get(IDserv1).add(IDpet2);
+        this.servidors.get(IDserv2).remove(IDpet2);
+        this.servidors.get(IDserv2).add(IDpet1);
     }
 }
