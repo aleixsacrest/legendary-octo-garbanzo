1f737c9a834c0f4442cce4aab179b830d4c9f34b
alexmiro9 <alexmiromed@gmail.com>
2016-04-03 19:48:53 +0200

    experiments 1 i 2

--- src/es/upc/fib/ia/experiments/Experiment1.java
+++ src/es/upc/fib/ia/experiments/Experiment1.java
@@ -18,7 +18,7 @@ public class Experiment1 extends Component {
         try {
             for (int i = 0; i < 10; ++i) {
 
-                int llavor = (int) (Math.random() * 100.);
+                int llavor = (int) 28;
                 Requests req = new Requests(200, 5, llavor);
                 Servers serv = new Servers(50, 5, llavor);
 
@@ -32,7 +32,7 @@ public class Experiment1 extends Component {
                 Estat e4 = new Estat(e1);
 
                 //HC: HF1 + SF1 (sense intercanviarAssig)
-                System.out.println("\nHC: HF1 + SF1 (sense intercanviarAssig)");
+                /*System.out.println("\nHC: HF1 + SF1 (sense intercanviarAssig)");
                 System.out.println("---------------------------------------");
                 System.out.print("Estat inicial" + e1);
                 DistFSHillClimbing(e1, 1, 0);
@@ -47,13 +47,13 @@ public class Experiment1 extends Component {
                 System.out.println("\nHC: HF2 + SF1 (sense intercanviarAssig)");
                 System.out.println("---------------------------------------");
                 System.out.print("Estat inicial" + e3);
-                DistFSHillClimbing(e3, 0, 0);
+                DistFSHillClimbing(e3, 0, 0);*/
 
                 //HC: HF2 + SF2 (amb intercanviarAssig)
                 System.out.println("\nHC: HF2 + SF2 (amb intercanviarAssig)");
                 System.out.println("---------------------------------------");
                 System.out.print("Estat inicial" + e4);
-                DistFSHillClimbing(e4, 1, 1);
+                DistFSHillClimbing(e4, 0, 1);
             }
 
 
@@ -61,71 +61,6 @@ public class Experiment1 extends Component {
             e.printStackTrace();
         }
     }
-
-    public static int inicialitzacio() {
-        Object[] possibilities = {"initMinTemps", "initEqCarrega", "initRandom"};
-        Component c = new Component() {
-            @Override
-            public String getName() {
-                return super.getName();
-            }
-        };
-        int st = (int) JOptionPane.showOptionDialog(
-                c, "Escull la manera d'inicialitzar estat",
-                "init", JOptionPane.OK_OPTION, JOptionPane.PLAIN_MESSAGE,
-                null, possibilities, null);
-        /*String s = (String) JOptionPane.showInputDialog(
-                c,"Escull la manera d'inicialitzar estat", "init",
-                JOptionPane.PLAIN_MESSAGE,
-                null, possibilities, null);*/
-        return st;
-    }
-
-    public static int algorisme() {
-        Object[] possibilities = {"Hill Climbing", "Simulated Annealing"};
-        Component c = new Component() {
-            @Override
-            public String getName() {
-                return super.getName();
-            }
-        };
-        int st = (int) JOptionPane.showOptionDialog(
-                c, "Escull l'algorisme a utilitzar",
-                "algorisme", JOptionPane.OK_OPTION, JOptionPane.PLAIN_MESSAGE,
-                null, possibilities, null);
-        return st;
-    }
-
-    public static int heuristicF() {
-        Object[] possibilities = {"heuristic2:\nmin temps + factor de carrega", "heuristic1:\nminimitzar pitjor servidor"};
-        Component c = new Component() {
-            @Override
-            public String getName() {
-                return super.getName();
-            }
-        };
-        int st = (int) JOptionPane.showOptionDialog(
-                c, "Escull la funcio heuristica a utilitzar",
-                "heurF", JOptionPane.OK_OPTION, JOptionPane.PLAIN_MESSAGE,
-                null, possibilities, null);
-        return st;
-    }
-
-    public static int successorF() {
-        Object[] possibilities = {"SuccessorFunction (canviar)", "SuccessorFunction2 (canviar+intercanviar)"};
-        Component c = new Component() {
-            @Override
-            public String getName() {
-                return super.getName();
-            }
-        };
-        int st = (int) JOptionPane.showOptionDialog(
-                c, "Escull la successorfunction a utilitzar",
-                "succesF", JOptionPane.OK_OPTION, JOptionPane.PLAIN_MESSAGE,
-                null, possibilities, null);
-        return st;
-    }
-
 
     private static void DistFSHillClimbing (Estat estat, int heurF, int succeFunc) {
         try {
@@ -139,29 +74,6 @@ public class Experiment1 extends Component {
             else succe = new DistFSSuccessorFunctionHC2();
             problem = new Problem(estat, succe, new DistFSGoalTest(), heur);
             Search search = new HillClimbingSearch();
-            SearchAgent agent = new SearchAgent(problem, search);
-            System.out.println();
-            printActions(agent.getActions());
-            System.out.println("---");
-            printInstrumentation(agent.getInstrumentation());
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void DistFSSimulatedAnnealing (Estat estat, int heurF, int succeFunc) {
-        try {
-
-            Problem problem;
-            HeuristicFunction heur;
-            SuccessorFunction succe;
-            if (heurF == 0) heur = new DistFSHeuristicFunction2();
-            else heur = new DistFSHeuristicFunction1();
-            if (succeFunc == 0) succe = new DistFSSuccessorFunctionSA();
-            else succe = new DistFSSuccessorFunctionSA2();
-            problem = new Problem(estat, succe, new DistFSGoalTest(), heur);
-            Search search = new SimulatedAnnealingSearch();
             SearchAgent agent = new SearchAgent(problem, search);
             System.out.println();
             printActions(agent.getActions());
--- src/es/upc/fib/ia/experiments/Experiment2.java
+++ src/es/upc/fib/ia/experiments/Experiment2.java
@@ -70,71 +70,6 @@ public class Experiment2 extends Component {
         }
     }
 
-    public static int inicialitzacio() {
-        Object[] possibilities = {"initMinTemps", "initEqCarrega", "initRandom"};
-        Component c = new Component() {
-            @Override
-            public String getName() {
-                return super.getName();
-            }
-        };
-        int st = (int) JOptionPane.showOptionDialog(
-                c, "Escull la manera d'inicialitzar estat",
-                "init", JOptionPane.OK_OPTION, JOptionPane.PLAIN_MESSAGE,
-                null, possibilities, null);
-        /*String s = (String) JOptionPane.showInputDialog(
-                c,"Escull la manera d'inicialitzar estat", "init",
-                JOptionPane.PLAIN_MESSAGE,
-                null, possibilities, null);*/
-        return st;
-    }
-
-    public static int algorisme() {
-        Object[] possibilities = {"Hill Climbing", "Simulated Annealing"};
-        Component c = new Component() {
-            @Override
-            public String getName() {
-                return super.getName();
-            }
-        };
-        int st = (int) JOptionPane.showOptionDialog(
-                c, "Escull l'algorisme a utilitzar",
-                "algorisme", JOptionPane.OK_OPTION, JOptionPane.PLAIN_MESSAGE,
-                null, possibilities, null);
-        return st;
-    }
-
-    public static int heuristicF() {
-        Object[] possibilities = {"heuristic2:\nmin temps + factor de carrega", "heuristic1:\nminimitzar pitjor servidor"};
-        Component c = new Component() {
-            @Override
-            public String getName() {
-                return super.getName();
-            }
-        };
-        int st = (int) JOptionPane.showOptionDialog(
-                c, "Escull la funcio heuristica a utilitzar",
-                "heurF", JOptionPane.OK_OPTION, JOptionPane.PLAIN_MESSAGE,
-                null, possibilities, null);
-        return st;
-    }
-
-    public static int successorF() {
-        Object[] possibilities = {"SuccessorFunction (canviar)", "SuccessorFunction2 (canviar+intercanviar)"};
-        Component c = new Component() {
-            @Override
-            public String getName() {
-                return super.getName();
-            }
-        };
-        int st = (int) JOptionPane.showOptionDialog(
-                c, "Escull la successorfunction a utilitzar",
-                "succesF", JOptionPane.OK_OPTION, JOptionPane.PLAIN_MESSAGE,
-                null, possibilities, null);
-        return st;
-    }
-
-
     private static void DistFSHillClimbing (Estat estat, int heurF, int succeFunc) {
         try {
 
@@ -147,29 +82,6 @@ public class Experiment2 extends Component {
             else succe = new DistFSSuccessorFunctionHC2();
             problem = new Problem(estat, succe, new DistFSGoalTest(), heur);
             Search search = new HillClimbingSearch();
-            SearchAgent agent = new SearchAgent(problem, search);
-            System.out.println();
-            printActions(agent.getActions());
-            System.out.println("---");
-            printInstrumentation(agent.getInstrumentation());
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void DistFSSimulatedAnnealing (Estat estat, int heurF, int succeFunc) {
-        try {
-
-            Problem problem;
-            HeuristicFunction heur;
-            SuccessorFunction succe;
-            if (heurF == 0) heur = new DistFSHeuristicFunction2();
-            else heur = new DistFSHeuristicFunction1();
-            if (succeFunc == 0) succe = new DistFSSuccessorFunctionSA();
-            else succe = new DistFSSuccessorFunctionSA2();
-            problem = new Problem(estat, succe, new DistFSGoalTest(), heur);
-            Search search = new SimulatedAnnealingSearch();
             SearchAgent agent = new SearchAgent(problem, search);
             System.out.println();
             printActions(agent.getActions());
